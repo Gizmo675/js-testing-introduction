aa62fb8493d040e69fd3a7ac20b27393398c276f
Romain <septier.romain@gmail.com>
2020-10-19 14:31:39 +0200

    testing with puppeteer

--- util.test.js
+++ util.test.js
@@ -1,4 +1,5 @@
 const { generateText, checkAndGenerate } = require('./util')
+const puppeteer = require('puppeteer')
 
 test('doit retourner un nom et un age', ()=>{
   const text = generateText('Romain', 33)
@@ -8,4 +9,21 @@ test('doit retourner un nom et un age', ()=>{
 test('doit generer un text valide', ()=>{
   const text = checkAndGenerate('Max', 29)
   expect(text).toBe('Max (29 years old)')
+})
+
+test('Doit créer un element avec le nom et la classe correspondantes', async ()=>{
+  const browser = await puppeteer.launch({
+    headless: false,
+    // slowMo: 80,
+    // args: ['--window-size=1920,1080']
+  })
+  const page = await browser.newPage()
+  await page.goto('file:///C:/Users/Gizmo/Documents/GitHub/js-testing-introduction/index.html')
+  await page.click('input#name')
+  await page.type('input#name', 'Anna')
+  await page.click('input#age')
+  await page.type('input#age', '29')
+  await page.click('#btnAddUser')
+  const finalText = await page.$eval('.user-item', el=>el.textContent)
+  expect(finalText).toBe('Anna (29 years old)')
 })
