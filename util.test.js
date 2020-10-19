0cc32068a6c121669cb8c69b72ce775874733105
Romain <septier.romain@gmail.com>
2020-10-19 14:09:00 +0200

    testing function with jest

--- util.test.js
+++ util.test.js
@@ -1,6 +1,11 @@
-const { generateText } = require('./util')
+const { generateText, checkAndGenerate } = require('./util')
 
 test('doit retourner un nom et un age', ()=>{
   const text = generateText('Romain', 33)
   expect(text).toBe('Romain (33 years old)')
+})
+
+test('doit generer un text valide', ()=>{
+  const text = checkAndGenerate('Max', 29)
+  expect(text).toBe('Max (29 years old)')
 })
